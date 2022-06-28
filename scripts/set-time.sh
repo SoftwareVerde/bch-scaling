377456da0aac9fbe76b1e4c91262064f271f41bb
Andrew Groot <groot@softwareverde.com>
2022-06-28 15:49:53 -0400

    Updated set-time.sh to work on Linux.

--- scripts/set-time.sh
+++ scripts/set-time.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-sudo systemsetup -setusingnetworktime Off 2>/dev/null
-sudo systemsetup -setdate 04/27/22 2>/dev/null
-sudo systemsetup -settime 22:00:00 2>/dev/null
-
+if [[ "$(uname)" -eq "Linux" ]]; then
+        sudo date --set 2022-04-27
+        sudo date --set 22:00:00
+else
+        sudo systemsetup -setusingnetworktime Off 2>/dev/null
+        sudo systemsetup -setdate 04/27/22 2>/dev/null
+        sudo systemsetup -settime 22:00:00 2>/dev/null
+fi
